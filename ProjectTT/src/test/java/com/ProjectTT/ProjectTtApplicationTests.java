26c30f4ce270af1c2dd146922242b6168b24f0ec
kimsia0717 <https://Kimsia0717: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@github.com/Kimsia0717/teamC_campYo>
2022-02-17 14:12:34 +0900

    ProjectTT

---
+++ ProjectTT/src/test/java/com/ProjectTT/ProjectTtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectTT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
